4692c43da329096a29c88e0bf6a81ac3a7db33cb
Alejandro Palacios Arévalo <alejandro.palacios88@gmail.com>
2024-12-11 15:14:33 -0500

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
+import { useState } from "react";
+
 import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
 
 const navigation = [
   { name: "Nuestros Productos", href: "#" },
